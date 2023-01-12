1797a19dceb0541cd7f81574c4f88de4b7a73e83
rushabh-indianic <110153864+rushabh-indianic@users.noreply.github.com>
2023-01-12 19:46:20 +0530

    Lumen compatible routes

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,17 @@
 <?php
 
 Route::group(config('translation.route_group_config') + ['namespace' => 'JoeDixon\\Translation\\Http\\Controllers'], function ($router) {
-    $router->get(config('translation.ui_url'), 'LanguageController@index')
-        ->name('languages.index');
+    $router->get(config('translation.ui_url'), 'LanguageController@index');
 
-    $router->get(config('translation.ui_url').'/create', 'LanguageController@create')
-        ->name('languages.create');
+    $router->get(config('translation.ui_url').'/create', 'LanguageController@create');
 
-    $router->post(config('translation.ui_url'), 'LanguageController@store')
-        ->name('languages.store');
+    $router->post(config('translation.ui_url'), 'LanguageController@store');
 
-    $router->get(config('translation.ui_url').'/{language}/translations', 'LanguageTranslationController@index')
-        ->name('languages.translations.index');
+    $router->get(config('translation.ui_url').'/{language}/translations', 'LanguageTranslationController@index');
 
-    $router->post(config('translation.ui_url').'/{language}', 'LanguageTranslationController@update')
-        ->name('languages.translations.update');
+    $router->post(config('translation.ui_url').'/{language}', 'LanguageTranslationController@update');
 
-    $router->get(config('translation.ui_url').'/{language}/translations/create', 'LanguageTranslationController@create')
-        ->name('languages.translations.create');
+    $router->get(config('translation.ui_url').'/{language}/translations/create', 'LanguageTranslationController@create');
 
-    $router->post(config('translation.ui_url').'/{language}/translations', 'LanguageTranslationController@store')
-        ->name('languages.translations.store');
+    $router->post(config('translation.ui_url').'/{language}/translations', 'LanguageTranslationController@store');
 });
